25d5a27b8d8d09de068979bbcfce297300067857
Osmar Leandro <osmarleandrods@gmail.com>
2018-08-09 17:54:18 -0300

    Error in brute force: change array variable

--- greedy-search/rdata.cpp
+++ greedy-search/rdata.cpp
@@ -15,9 +15,14 @@ using std::ofstream;
 #include <cmath>
 using std::pow;
 
+#include <array>
+using std::array;
+
 int **reading();
 void see(int **adj, int n);
-void brute_force(int **adj, int n);
+int *brute_force(int **adj, int n);
+int cost(int **adj, int *sol, int n);
+int *swap(int *sol, int i, int j);
 
 int main()
 {
@@ -56,6 +61,8 @@ int **reading()
     cout << "Unable to open file";
 
   see(adj, n);
+  brute_force(adj, n);
+
   return adj;
 }
 
@@ -71,25 +78,48 @@ void see(int **adj, int n)
   }
 }
 
-void brute_force(int **adj, int n)
+int *brute_force(int **adj, int n)
 {
-  int cost = INFINITY;
-  int sol[] = {};
+  // initial solution and cost
+  int bestSol[] = { 0, 1, 2, 3, 4 };
+  
+  int bestCost = 17;
 
   // calculing cycle cost
-  for (int i = 0; i < n; i++)
-  {
-    for (int j = 0; j < n; j++)
-    {
-    }
-  }
-}
+  int total = cost(adj, bestSol, n);
+  cout << "Solution cost: " << total << endl;
 
-void cost(int **adj, int *sol, int n)
-{
-  int total_cost = 0;
+  // swapping every city and calculating cost
   for (int i = 0; i < n - 1; i++)
   {
-    total_cost = total_cost + adj[sol[i]][sol[i + 1]];
+    for (int j = i + 1; j < n; j++)
+    {
+      int *current = swap(bestSol, i, j);
+      if (cost(adj, current, n) < bestCost)
+        bestSol = *current;
+    }
   }
+  return bestSol;
+}
+
+int cost(int **adj, int *sol, int n)
+{
+  int total_cost = 0;
+  for (int i = 0; i <= n - 1; i++)
+  {
+    // if i is the last index, j equals to first: cost of first city return
+    int j = (i == n - 1) ? 0 : (i + 1);
+
+    total_cost = total_cost + adj[sol[i]][sol[j]];
+    cout << "From " << sol[i] << " To " << sol[j] << " = " << adj[sol[i]][sol[j]] << endl;
+  }
+  return total_cost;
+}
+
+int *swap(int *sol, int i, int j)
+{
+  int tmp = sol[i];
+  sol[i] = sol[j];
+  sol[j] = tmp;
+  return sol;
 }
