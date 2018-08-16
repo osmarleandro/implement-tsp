91118322031b52edca73c712e552219ec7713983
Osmar Leandro <osmarleandrods@gmail.com>
2018-08-15 22:41:15 -0300

    [dev] next closest neighbor

--- greedy-algorithm/main.cpp
+++ greedy-algorithm/main.cpp
@@ -1,37 +1,87 @@
 #include "rdata.h"
-#include <fstream>
+
 #include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
 
-using namespace std;
+#include <fstream>
+using std::ifstream;
+using std::ofstream;
 
-double **matrixAdj; // matriz de adjacencia
-int dimension;      // quantidade total de vertices
+#include <algorithm>
+using std::find;
 
 void see(double **matrix, int n);
 void see(int *vector, int n);
 
+int *greedy(double **adj, int dimension, int start);
+bool visited(const int *begin, const int *end, int city);
+
 int main(int argc, char **argv)
 {
 
+  double **matrixAdj;
+  int dimension;
+
   readData(argc, argv, &dimension, &matrixAdj);
   see(matrixAdj, dimension);
+
+  // Deallocate memory
+  delete[] matrixAdj;
   return 0;
 }
 
-void see(double **matrix, int n)
+void see(double **matrix, int dimension)
 {
-  cout << "The Matrix of " << n << " dimensions" << endl;
-  for (size_t i = 1; i <= n; i++)
+  cout << "The Matrix of " << dimension << " dimensions" << endl;
+  for (size_t i = 1; i <= dimension; i++)
   {
-    for (size_t j = 1; j <= n; j++)
+    for (size_t j = 1; j <= dimension; j++)
       cout << matrix[i][j] << "\t";
     cout << endl;
   }
 }
 
-void see(int *vector, int n)
+void see(int *vector, int dimension)
 {
-  for (int i = 0; i < n; i++)
+  for (int i = 0; i < dimension; i++)
     cout << vector[i] << "\t";
   cout << endl;
+}
+
+int *greedy(double **adj, int dimension, int start)
+{
+  int *path = new int[dimension];
+  int *end = path + dimension;
+  int current = start;
+
+  do
+  {
+    *path = current;
+    int next = nextCloser(adj, dimension, current, path);
+
+    current = next;
+    ++path;
+
+  } while (path != end);
+
+  return path;
+}
+
+bool visited(const int *begin, const int *end, int city)
+{
+  return find(begin, end, city) != end;
+}
+
+int nextCloser(double **adj, int dimension, int city, int *path)
+{
+  int neighbor = dimension + 1;
+  while (!visited(path, path + dimension, adj[city][neighbor]))
+  {
+
+    neighbor++;
+  }
+
+  return neighbor;
 }
