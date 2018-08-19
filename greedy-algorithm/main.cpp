0bdac8fab44add5e91d2b348b01d046a6e0d25f9
Osmar Leandro <osmarleandrods@gmail.com>
2018-08-19 01:09:18 -0300

    Add brute force function

--- greedy-algorithm/main.cpp
+++ greedy-algorithm/main.cpp
@@ -12,45 +12,61 @@ using std::ifstream;
 using std::ofstream;
 
 #include <algorithm>
+using std::copy;
 using std::find;
+using std::next_permutation;
+using std::sort;
 
 #include <limits>
 using std::numeric_limits;
 
-void timeExecution(clock_t startTime);
+double timeExecution(clock_t startTime);
 void see(double **matrix, int dimension);
 void see(int *vector, int dimension);
-void seeCost(int *path, int dimension, double **matrix);
+double cost(int *path, int dimension, double **matrix);
 
 int *greedy(double **adj, int dimension, int start);
 int bestNeighbor(double **adj, int dimension, int *path, int city);
 bool visited(const int *begin, const int *end, int city);
+int *brute_force(double **adj, int n, int *currentSolution);
 
 int main(int argc, char **argv)
 {
   double **matrixAdj;
   int dimension;
+  int startCity = 1;
 
   // Reading the data of instance passed by parameter
   readData(argc, argv, &dimension, &matrixAdj);
 
-  int startCity = 666;
-  clock_t tStart = clock();
+  // Starting greedy algorithm looking for the best neighbor
+  clock_t startTimeGreedy = clock();
   int *path = greedy(matrixAdj, dimension, startCity);
+  double totalTime = timeExecution(startTimeGreedy);
+
+  see(path, dimension);
+  double totalCost = cost(path, dimension, matrixAdj);
 
   // Show the solution path
-  see(path, dimension);
-  seeCost(path, dimension, matrixAdj);
-  timeExecution(tStart);
+  cout << "The total cost of path was " << totalCost << endl;
+  cout << "Tempo de execução: " << totalTime << " s" << endl;
+
+  clock_t startTimeBrute = clock();
+  int *bestPath = brute_force(matrixAdj, dimension, path);
+  totalTime = timeExecution(startTimeBrute);
+
+  double bestCost = cost(bestPath, dimension, matrixAdj);
+  cout << "The best cost of path is " << bestCost << endl;
+  cout << "Tempo de execução: " << totalTime << " s" << endl;
 
   // Deallocate memory
   delete[] matrixAdj, path;
   return 0;
 }
 
-void timeExecution(clock_t startTime)
+double timeExecution(clock_t startTime)
 {
-  cout << "Tempo de execução: " << (double)(clock() - startTime) / CLOCKS_PER_SEC << " s" << endl;
+  return (double)(clock() - startTime) / CLOCKS_PER_SEC;
 }
 
 void see(double **matrix, int dimension)
@@ -76,7 +92,7 @@ void see(int *vector, int dimension)
   cout << endl;
 }
 
-void seeCost(int *path, int dimension, double **matrix)
+double cost(int *path, int dimension, double **matrix)
 {
   double totalCost = 0;
   for (size_t i = 0; i < dimension - 1; i++)
@@ -84,7 +100,7 @@ void seeCost(int *path, int dimension, double **matrix)
 
   // Add symmetric cost of last city to first city
   totalCost = totalCost + matrix[path[0]][path[dimension - 1]];
-  cout << "The total cost of path was " << totalCost << endl;
+  return totalCost;
 }
 
 int *greedy(double **adj, int dimension, int start)
@@ -125,4 +141,28 @@ int bestNeighbor(double **adj, int dimension, int *path, int city)
 bool visited(const int *begin, const int *end, int city)
 {
   return find(begin, end, city) != end;
+}
+
+int *brute_force(double **adj, int n, int *bestSol)
+{
+  double iter = 1;
+  double bestCost = cost(bestSol, n, adj);
+
+  int *current = new int[n];
+  copy(bestSol, bestSol + n, current);
+  sort(current, current + n);
+
+  while (next_permutation(current, current + n))
+  {
+    // calculing cost of current solution for next permutation
+    double currentCost = cost(current, n, adj);
+    if (currentCost < bestCost)
+    {
+      copy(current, current + n, bestSol);
+      bestCost = currentCost;
+    }
+    iter++;
+  };
+  cout << "Iterations: " << iter << endl;
+  return bestSol;
 }
